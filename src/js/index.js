9857c069c23e8ab27171824e09bb2a9653cf9c49
zhanghan <1119432095@qq.com>
2016-01-08 11:58:09 +0800

    增加背景图片和头部标题背景图片

--- src/js/index.js
+++ src/js/index.js
@@ -3,47 +3,43 @@ $(function() {
 	// 欢迎页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img:first').slideUp().next().slideDown();
-	}, 3000);
+	}, 10000);
 	// 登录页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(1).slideUp().next().slideDown();
-	}, 6000);
+	}, 20000);
 	// 发布页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(2).fadeOut().next().fadeIn();
-	}, 9000);
+	}, 30000);
 	// 发布页面2
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(3).fadeOut().next().fadeIn();
-	}, 12000);
-	// 列表页面
-	setTimeout(function() {
-		$('.j-purchaser').find('img').eq(4).fadeOut().next().fadeIn();
-		$('.j-vendor').find('img').eq(0).fadeOut().next().fadeIn();
-	}, 16000);
+		$('.j-vendor').find('img:first').fadeOut().next().fadeIn();
+	}, 40000);
 	// 供应商发布页面
 	setTimeout(function() {
 		$('.j-vendor').find('img').eq(1).fadeOut().next().fadeIn();
-	}, 18000);
+	}, 55000);
 	// 供应商发布页面2
 	setTimeout(function() {
 		$('.j-vendor').find('img').eq(2).fadeOut().next().fadeIn();
-	}, 21000);
-	// 列表页面2
+	}, 70000);
+	// 列表页面
 	setTimeout(function() {
-		$('.j-purchaser').find('img').eq(5).fadeOut().next().fadeIn();
-	}, 24000);
+		$('.j-purchaser').find('img').eq(4).fadeOut().next().fadeIn();
+	}, 85000);
 	// offer页面
 	setTimeout(function() {
-		$('.j-purchaser').find('img').eq(6).fadeOut().next().fadeIn();
-	}, 27000);
+		$('.j-purchaser').find('img').eq(5).fadeOut().next().fadeIn();
+	}, 100000);
 	// offer页面2
 	setTimeout(function() {
-		$('.j-purchaser').find('img').eq(7).fadeOut().next().fadeIn();
-	}, 30000);
+		$('.j-purchaser').find('img').eq(6).fadeOut().next().fadeIn();
+	}, 115000);
 	// 订单页面
 	setTimeout(function() {
-		$('.j-purchaser').find('img').eq(8).fadeOut().next().fadeIn();
+		$('.j-purchaser').find('img').eq(7).fadeOut().next().fadeIn();
 		$('.j-vendor').find('img').eq(3).fadeOut().next().fadeIn();
-	}, 33000);
+	}, 130000);
 });
