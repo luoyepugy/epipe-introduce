57287d3dc060d3b790168152ac8261105f2dbe12
zhanghan <1119432095@qq.com>
2016-01-12 16:11:09 +0800

    增加手机外壳图片

--- src/js/index.js
+++ src/js/index.js
@@ -3,39 +3,39 @@ $(function() {
 	// 欢迎页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img:first').slideUp().next().slideDown();
-	}, 15000);
+	}, 25000);
 	// 登录页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(1).slideUp().next().slideDown();
-	}, 30000);
+	}, 35000);
 	// 发布页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(2).fadeOut().next().fadeIn();
-	}, 50000);
+	}, 47000);
 	// 发布页面2
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(3).fadeOut().next().fadeIn();
 		$('.j-vendor').find('img:first').fadeOut().next().fadeIn();
-	}, 70000);
+	}, 60000);
 	// 供应商发布页面
 	setTimeout(function() {
 		$('.j-vendor').find('img').eq(1).fadeOut().next().fadeIn();
-	}, 90000);
+	}, 80000);
 	// 列表页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(4).fadeOut().next().fadeIn();
-	}, 110000);
+	}, 103000);
 	// offer页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(5).fadeOut().next().fadeIn();
-	}, 130000);
+	}, 113000);
 	// offer页面2
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(6).fadeOut().next().fadeIn();
-	}, 150000);
+	}, 125000);
 	// 订单页面
 	setTimeout(function() {
 		$('.j-purchaser').find('img').eq(7).fadeOut().next().fadeIn();
 		$('.j-vendor').find('img').eq(2).fadeOut().next().fadeIn();
-	}, 170000);
+	}, 135000);
 });
